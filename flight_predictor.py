dc87a078c8cb775f4e26c45c27efd8c31a9a874f
FranklinWrexham <Wrexhamlogin@gmail.com>
2026-04-07 15:46:50 +0100

    Enhance flight predictor with time selection inputs
    Added departure and arrival time selections to the UI and updated prediction logic to include these features.

--- flight_predictor.py
+++ flight_predictor.py
@@ -19,12 +19,15 @@ with col1:
     source = st.selectbox("Source City", ['Delhi', 'Mumbai', 'Bangalore', 'Kolkata', 'Hyderabad', 'Chennai'])
     destination = st.selectbox("Destination City", ['Mumbai', 'Delhi', 'Bangalore', 'Kolkata', 'Hyderabad', 'Chennai'])
     stops = st.selectbox("Stops", ['non-stop', '1-stop', '2+-stop'])
+    dep_time = st.selectbox("Departure Time", ['Before 6 AM', '6 AM - 12 PM', '12 PM - 6 PM', 'After 6 PM'])
+
 
 with col2:
     travel_class = st.selectbox("Class", ['Economy', 'Premium Economy', 'Business', 'First'])
     duration = st.number_input("Duration (Hours)", min_value=0.5, max_value=50.0, value=2.5)
     travel_date = st.date_input("Select Date of Journey", min_value=date.today())
-
+    arr_time = st.selectbox("Arrival Time", ['Before 6 AM', '6 AM - 12 PM', '12 PM - 6 PM', 'After 6 PM'])
+    
    # AUTOMATIC CALCULATIONS
     today = date.today()
     days_left = (travel_date - today).days
@@ -44,17 +47,19 @@ st.info(f"Calculated Days Left: {days_left}")
 # 3. The Prediction Logic (Your code goes here)
 if st.button("Predict Flight Price"):
     # Create the dictionary from user inputs
-    new_flight = {
-        'Airline': airline,
-        'Source': source,
-        'Destination': destination,
-        'Total_stops': stops,
-        'Class': travel_class,
-        'Duration_in_hours': duration,
-        'Days_left': days_left,
-        'Journey_day': day,
-        'Journey_month': month
-    }
+   new_flight = {
+    'Airline': airline,
+    'Source': source,
+    'Destination': destination,
+    'Total_stops': stops,
+    'Class': travel_class,
+    'Duration_in_hours': duration,
+    'Days_left': days_left,
+    'Journey_day': day,
+    'Journey_month': month,
+    'Departure_Time': dep_value, 
+    'Arrival_Time': arr_value    
+}
     
     # Process inputs
     input_df = pd.DataFrame([new_flight])
@@ -64,6 +69,16 @@ if st.button("Predict Flight Price"):
     class_map = {'Economy': 0, 'Premium Economy': 1, 'Business': 2, 'First':3}
     input_df['Total_stops'] = input_df['Total_stops'].map(stops_map)
     input_df['Class'] = input_df['Class'].map(class_map)
+    time_map = {
+    'Before 6 AM': 1,
+    '6 AM - 12 PM': 2,
+    '12 PM - 6 PM': 3,
+    'After 6 PM': 4
+}
+
+# Convert the text selection to the number your model expects
+dep_value = time_map[dep_time]
+arr_value = time_map[arr_time]
     
     # Encode and Align
     input_encoded = pd.get_dummies(input_df)
